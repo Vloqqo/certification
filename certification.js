6a99cef7644d1328d6f06d1e2c6f07772802f419
Vloqqo <vloqqo@gmail.com>
2025-02-04 14:51:46 -0800

    Starting

---
+++ certification.js
@@ -0,0 +1,24 @@
+let date_data= new Date()
+let localDate = date_data.toLocaleString();
+let current_hour = date_data.getHours()
+let current_minute = date_data.getMinutes();
+
+const certification1 = {};
+
+certification1.date = localDate;
+certification1.issuer = "";
+certification1.recepient = "";
+
+
+const certification2 = {};
+
+certification2.date = localDate;
+certification2.issuer = "";
+certification2.recepient = "";
+
+const certification3 = {};
+
+certification3.date = localDate;
+certification3.issuer = "";
+certification3.recepient = "";
+
