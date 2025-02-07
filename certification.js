11e905cd12c5e682beb498863a2cd3147f9c2b4f
Vloqqo <vloqqo@gmail.com>
2025-02-06 23:54:52 -0800

    Just need to figure out whats going wrong w/html

--- certification.js
+++ certification.js
@@ -5,22 +5,27 @@ let current_minute = date_data.getMinutes();
 // Defining variables for time
 
 
-
 // This makes it
 function enter(id) {
     const enterKey = document.getElementById(id);
     enterKey.addEventListener("keydown", (event) => {
         if (event.key === "Enter") {
             alert("Works!");
-            
-        }
-    })
-}
+            if (id == 'i1') {
+                value1 = document.getElementById('i1').value;
 
-function getvalues(id, value) {
-    value = document.getElementById(id).value;
-    // reset('Name', promptClick1())
-    return(value);
+                // localStorage.setItem('Name', value1);
+
+                return (value1);
+            }
+            if (id == 'i2') {
+                value2 = document.getElementById('i2').value;
+                localStorage.setItem('Issuer', value2);
+                return (value2)
+            }
+        }
+
+    })
 }
 
 // This creates an input element in the html
@@ -31,7 +36,7 @@ function promptClick1() {
     document.getElementById('b1').replaceWith(nameChoose);
     // This calls the enter function giving it the id to 
     enter(nameChoose.id);
-    getvalues(nameChoose.id, value);
+    // reset('Name', promptClick1());
 }
 
 // function reset(text, func) {
@@ -39,16 +44,15 @@ function promptClick1() {
 //     resetButton.id = ('b1');
 //     resetButton.innerHTML = text;
 //     resetButton.onclick = func
+//     document.getElementById('i1').replacedWith(resetButton)
 // }
 
-function promptClick2() {
-    let issuerCreate = document.createElement('input')
-    issuerCreate.id = ("i2");
-    document.getElementById('b2').replaceWith(issuerCreate)
-    enter(issuerCreate.id);
-    certification.issuer = value
-    getvalues(issuerCreate.id, value)
-}
+// function promptClick2() {
+//     let issuerCreate = document.createElement('input')
+//     issuerCreate.id = ("i2");
+//     document.getElementById('b2').replaceWith(issuerCreate)
+//     enter(issuerCreate.id);
+// }
 
 function promptClick(bnum) {
     let x = 0;
@@ -69,11 +73,47 @@ function promptClick(bnum) {
         promptClick3(x);
     }
 }
-const certification = {};
 
-certification.date = localDate;
-certification.issuer;
-certification.recepient;
 
-// localStorage.setItem(issuer, certification.issuer);
-// localStorage.setItem(recepient, certification.recepient)
+
+const cert1 = {};
+
+cert1.title = `Certified Henchman`;
+cert1.description = `the certification of completing his Henchman training. With this official title, you may now start going by a Henchman. With this certification you may not dabble in villainy, evildoing, nefarious deeds, and any activities related to being a goon for any Villain, Mastermind, Overlord, or Evildoer.`;
+cert1.issuer = `The Organization of Villainy`;
+
+const cert2 = {};
+
+cert2.title = `Astronaut Certification`;
+cert2.description = `completion of the Astronaut certification process. With this certificate they will now be considered a full-fledged astronaut. They will be apart of NASA, able to take on missions they so choose to help further Humanity and Science.`;
+cert2.issuer = `National Aeronautics and Space Administration`;
+
+const cert3 = {};
+
+cert3.title = `Computer Science Certification`;
+cert3.description = `has completed their training in being a certified Computer Science Developer. This certification can be used for priority at a nearby homeless shelter instead of having to fight others (Art Majors) for a spot in case of limited availability at the aforementioned homeless shelter`;
+cert3.issuer = `The Chamber of Computer Science`
+
+// timetell = document.querySelector('.time');
+// timetell.innerHTML += `<h4>Issued on: ${localDate}</h4>`;
+
+certIds = ['Henchman', 'csdevin2025', 'astronaut'];
+let certPage = document.querySelector(certIds);
+if (certPage == 'Henchman') {
+    certPage.insertAdjacentHTML('beforeend', `
+        <img src="/images/villainy.png" alt="Villainous Henchman">
+        <section class="villainy">
+            <img src="/images/shirt.png" alt="Hilarious Shirt btw" width="100px" height="100px">
+        </section>
+        <section class="cert-text">
+            <h2 class="line">${cert1.title}</h2>
+            <h2>In the utmost esteem that villainy can give. We bestow upon:</h2>
+            <h1 class="name">John Doe</h1>
+            <p>${cert1.description}</p>
+            <h3>${cert1.issuer}</h3>
+            <section class="time"></section>
+        </section>
+        <section class="villainy">
+            <img src="/images/lol.png" alt="Villainous Certification Stamp" width="200px" height="200px">
+        </section>`)
+};
